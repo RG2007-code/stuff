6f75da9730a102714665eaabef9263ca2ece355b
RG2007-code <nozhenko.egor@yandex.ru>
2025-10-18 18:31:50 +0700

    Add files via upload
    авторизация wpf форма

---
+++ авторизация/авторизация/PageLogin.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace авторизация
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public Frame mainFrame;
+
+        public PageLogin(Frame frame)
+        {
+            InitializeComponent();
+            mainFrame = frame;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbLogin.Text;
+            string password = pbPassword.Password;
+
+            if (login == "admin" && password == "1234") //если логин admin и пароль 1234 то авторизация проходит успешно и  открывается страница PageWelcome 
+            {
+                mainFrame.Content = new PageWelcome();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!");
+            }
+        }
+    }
+}
